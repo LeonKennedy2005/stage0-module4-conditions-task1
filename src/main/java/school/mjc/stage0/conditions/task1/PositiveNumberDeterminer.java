c221845cda21d5f08bc0f3dbc624771ee28fc891
LeonKennedy2005 <139060906+LeonKennedy2005@users.noreply.github.com>
2023-09-03 18:21:27 +0600

    one more fix

--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -4,13 +4,13 @@ public class PositiveNumberDeterminer {
     public void isPositive(int numberToBeDetermined) {
         if (numberToBeDetermined>0){
             System.out.println("true");
-        } else if (numberToBeDetermined<=0) {
+        } else {
             System.out.println("false");
         }
     }
     public static void main(String[] args) {
         PositiveNumberDeterminer determiner = new PositiveNumberDeterminer();
-        int numberToBeDetermined =0;
+        int numberToBeDetermined =-7;
         determiner.isPositive(numberToBeDetermined);
     }
 }
